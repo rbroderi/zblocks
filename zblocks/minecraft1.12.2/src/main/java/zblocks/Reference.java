7de63cdc397ee197fa333b73dde61a53db72d902
rbroderi <richard@.sanguinesoftware.com>
2020-04-18 21:57:08 -0400

    updated version

--- zblocks/minecraft1.12.2/src/main/java/zblocks/Reference.java
+++ zblocks/minecraft1.12.2/src/main/java/zblocks/Reference.java
@@ -2,7 +2,7 @@ package zblocks;
 
 public class Reference {
 	public static final String MODID = "zblock";
-	public static final String MODNAME = "Zelda Style Pushable Puzzle Blocks";
-	public static final String VERSION = "1.0";
+	public static final String MODNAME = "Pushable Puzzle Blocks";
+	public static final String VERSION = "1.2020.04.18";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
 }
