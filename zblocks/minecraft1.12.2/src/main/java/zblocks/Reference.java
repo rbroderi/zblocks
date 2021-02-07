482ac14922335f3a7b9429bc0c6e14afb75defdb
rbroderi <richard@.sanguinesoftware.com>
2021-02-06 23:45:00 -0500

    updated version

--- zblocks/minecraft1.12.2/src/main/java/zblocks/Reference.java
+++ zblocks/minecraft1.12.2/src/main/java/zblocks/Reference.java
@@ -3,7 +3,7 @@ package zblocks;
 public class Reference {
 	public static final String MODID = "zblock";
 	public static final String MODNAME = "Pushable Puzzle Blocks";
-	public static final String VERSION = "1.2020.05.10";
+	public static final String VERSION = "1.2021.02.06";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
 	public static final int AREA_JUKEBOX_GUI = 123456;
 }
